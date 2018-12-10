f6794fca8e4ab93405e7f361f9cbe39e3fb12e56
jneill775 <neilljs@rose-hulman.edu>
2018-12-10 10:36:09 -0500

    YEET did problem 2

--- src/problem2.py
+++ src/problem2.py
@@ -3,8 +3,8 @@ PRACTICE Exam 1, problem 2.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Valerie Galluzzi, Mark Hays, Amanda Stouder, Aaron Wilkin,
-         their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues, and John Neill.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -31,7 +31,7 @@ import rosegraphics as rg
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_problem2a()
+    #run_test_problem2a()
     run_test_problem2b()
 
 
@@ -74,6 +74,23 @@ def run_test_problem2a():
 
 
 def problem2a(circle, rectangle, window):
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    window.render()
+    window.get_next_mouse_click()
+
+    yagayeet = rg.Line(rectangle.get_upper_right_corner(), rectangle.get_lower_left_corner())
+    yagayeet.arrow = 'last'
+    yagayeet.attach_to(window)
+    window.render()
+    window.get_next_mouse_click()
+
+    circle.fill_color = rectangle.outline_color
+    window.render()
+
+
+
+
     """
     See   problem2a_picture.pdf   in this project for pictures
     that may help you better understand the following specification:
@@ -103,7 +120,7 @@ def problem2a(circle, rectangle, window):
       :type window:    rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
@@ -145,6 +162,15 @@ def run_test_problem2b():
 
 
 def problem2b(rect, n, delta, win):
+    rect.attach_to(win)
+    win.render()
+    for k in range(n):
+        градславныйлидерсталин = rg.Rectangle(rg.Point(rect.get_lower_left_corner().x - (k * delta), rect.get_lower_left_corner().y + (k * delta)), rg.Point(rect.get_upper_right_corner().x + (k * delta), rect.get_upper_right_corner().y - (k * delta)))
+        градславныйлидерсталин.attach_to(win)
+        win.render()
+
+
+
     """
     See   problem2b_picture.pdf   in this project for pictures
     that may help you better understand the following specification:
@@ -174,7 +200,7 @@ def problem2b(rect, n, delta, win):
       :type win:    rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
